583473621ee203ab8206179e6c34f3b43b753f89
busra <busrahyt42@gmail.com>
2023-05-07 23:28:39 +0300

    SimpleGETTest class has been updated

--- src/test/java/apiTest/day01/SimpleGETTest.java
+++ src/test/java/apiTest/day01/SimpleGETTest.java
@@ -1,8 +1,14 @@
 package apiTest.day01;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.*; //içindeki tüm metodlar static, kolaylık olsun diye bu şekilde yazabilirsin.
+import static io.restassured.RestAssured.*;
+
 
 public class SimpleGETTest {
 
@@ -19,5 +25,44 @@ public class SimpleGETTest {
         response.prettyPrint();
     }
 
+    @Test
+    public void test2() {
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .header("Accept", ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(petStoreBaseURL + "/store/inventory");
 
+        Assert.assertEquals(response.statusCode(),200);
+        Assert.assertEquals(response.contentType(),"application/json");
+    }
+
+    @Test
+    public void test3() {
+        //resassured library kullanarak assert yapalım.
+
+        RestAssured.given().accept(ContentType.JSON)
+                .header("Accept", ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(petStoreBaseURL + "/store/inventory")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .contentType("application/json");
+    }
+
+    @Test
+    public void test4() {
+        Response response = given().accept(ContentType.JSON)
+                .when()
+                .get(petStoreBaseURL + "/store/inventory");
+
+        assertEquals(response.statusCode(),200);
+        assertEquals(response.contentType(),"application/json");
+
+        assertTrue(response.body().asString().contains("pending"));
+
+    }
 }
